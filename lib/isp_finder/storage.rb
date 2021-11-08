8fc7a3442063d0e764873be7f77acf50db358b10
Kevin McCormack <harlemsquirrel@mccormack.tech>
2021-11-08 17:31:21 -0500

    Update Storage

--- lib/isp_finder/storage.rb
+++ lib/isp_finder/storage.rb
@@ -8,34 +8,42 @@ module ISPFinder
 
     class << self
       def delete(key)
-        store.transaction { store.delete(key) }
+        new.delete(key)
       end
 
       def fetch(key, proc)
-        return proc.call if testing?
-
-        value = read(key)
-        return value if value
-
-        # Load the resources outside the store transaction so we don't lock while performing
-        # network requests
-        value = proc.call
-        store.transaction { store[key] ||= value }
+        new.fetch(key, proc)
       end
+    end
 
-      private
+    def delete(key)
+      store.transaction { store.delete(key) }
+    end
 
-      def read(key)
-        PStore.new(FILE_NAME, true).tap { |rstore| rstore.transaction(true) { return rstore[key] } }
-      end
+    def fetch(key, proc)
+      return proc.call if testing?
 
-      def store
-        @store ||= PStore.new(FILE_NAME, true)
-      end
+      value = read(key)
+      return value if value
 
-      def testing?
-        defined?(ISP_FINDER_ENV) && ISP_FINDER_ENV == 'test'
-      end
+      # Load the resources outside the store transaction so we don't lock while performing
+      # network requests
+      value = proc.call
+      store.transaction { store[key] = value }
+    end
+
+    private
+
+    def read(key)
+      store.transaction(true) { return store[key] }
+    end
+
+    def store
+      @store ||= PStore.new(FILE_NAME, true)
+    end
+
+    def testing?
+      defined?(ISP_FINDER_ENV) && ISP_FINDER_ENV == 'test'
     end
   end
 end
